4602de1dfaee96b5a51f8b15b6c9d751f829dbb0
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-09-01 14:43:00 +0200

    fixed issues

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const brandName = document.querySelector(".bi-house-door-fill");
+const homeIcon = document.querySelector(".bi-house-door-fill");
 function redirectButtons() {
+  location.href = "feed.html";
+};
+
+homeIcon.addEventListener("click", redirectButtons);
+
+const brandName = document.querySelector(".brandName");
+function redirectButtonsLogin() {
   location.href = "index.html";
 };
 
-brandName.addEventListener("click", redirectButtons);
+brandName.addEventListener("click", redirectButtonsLogin);
 
-console.log("hello");
-console.log(brandName);
+
 
 
